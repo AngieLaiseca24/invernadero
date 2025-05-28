86556443d04a59b2f8acaf840598561be168efbf
Angie Laiseca <anguielaiseca123@gmail.com>
2025-05-28 08:32:48 -0500

    Add: save image base 64 and jpg

--- app/db.py
+++ app/db.py
@@ -13,4 +13,11 @@ def init_db():
 
 def get_db():
     global client
+
+    if client is None:
+        MONGO_URI = os.getenv("MONGO_URI")
+        if not MONGO_URI:
+            raise RuntimeError("❌ MONGO_URI no está definido en el archivo .env")
+        client = MongoClient(MONGO_URI)
+
     return client["invernadero_db"]
